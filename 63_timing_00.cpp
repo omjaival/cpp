de844b58006f15e4b1778bc4f3bf686a0373e185
omjaival <omjaivalnvs@gmail.com>
2026-06-17 20:23:19 +0530

    duration of code

---
+++ 63_timing_00.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+
+void Function() {
+    for (int i = 0; i < 100; i++)
+        std::cout << "Hello" << std::endl;
+}
+
+int main() {
+    using namespace std::literals::chrono_literals;
+
+    auto start = std::chrono::high_resolution_clock::now();
+    std::this_thread::sleep_for(1s);
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<float> duration = end - start;
+    std::cout << duration.count() << "s " << std::endl;
+
+    std::cin.get();
+}
