37576691605d1df1bc96cdc16398202cc2dd646c
omjaival <omjaivalnvs@gmail.com>
2026-06-17 20:27:24 +0530

    demonstrating 2-d & 3-d array

---
+++ 67_multidimentional _array_02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int **a2d = new int *[50];
+    for (int i = 0; i < 50; i++)
+        a2d[i] = new int[50];
+
+    int ***a3d = new int **[50];
+    for (int i = 0; i < 50; i++) {
+        a3d[i] = new int *[50];
+        for (int j = 0; j < 50; j++) {
+            int **ptr = a3d[i];
+            ptr[j] = new int[50];
+        }
+    }
+
+    std::cin.get();
+}
