db49014d2e150c27af3926629a3517804e056438
omjaival <omjaivalnvs@gmail.com>
2026-08-19 23:00:49 +0530

    corrected errors and usage of const

--- 17_inheritance.cpp
+++ 17_inheritance.cpp
@@ -10,21 +10,20 @@ class Entity
         X  += xa;
         Y  += ya;
     }
-}
+};
 
 class player : public Entity
 {
     public:
     const char* Name;
 
-    void PrintName()
-    {
+    [[maybe_unused]] void PrintName() const {
         std::cout<<Name<<std::endl;
     }
 };
 int main()
 {
-    player player;
+    player player{};
     player.Move(5,5);
     player.X=2;
 
