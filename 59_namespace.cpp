c67c3cd513f6bfd60b581a4cc942a7ef1bc87f5a
omjaival <omjaivalnvs@gmail.com>
2026-08-21 23:55:01 +0530

    updated the namespace using vector

--- 59_namespace.cpp
+++ 59_namespace.cpp
@@ -5,21 +5,22 @@
 
 using namespace std;
 
-void ForEach(const  vector<int>& values, const function<void(int)>& func)
-{
-    for (int value : values)
+void ForEach(const vector<int> &values, const function<void(int)> &func) {
+    for (int value: values)
         func(value);
 }
 
-int main()
-{
-    vector<int> values = {1, 5, 4, 2, 3 };
-    auto it = find_if(values.begin(), values.end(),[](int value) {return value > 3;});
+int main() {
+    vector<int> values = {1, 5, 4, 2, 3};
+    auto it = find_if(
+        values.begin(),
+        values.end(),
+        [](int value) { return value > 3; });
     cout << *it << endl;
 
     int a = 5;
 
-    auto lambda = [a](int value){ cout << "Value: " << a <<  endl; };
+    auto lambda = [a](int value) { cout << "Value: " << a << endl; };
 
     ForEach(values, lambda);
     cin.get();
